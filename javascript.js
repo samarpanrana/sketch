f2c17fcb104dba6784065d94326dede020948d3d
Samarpan <164048288+samarpanrana@users.noreply.github.com>
2024-03-27 19:51:35 +0545

    Add darken shade on hover feature

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ let columns = document.querySelector('#columns').value;
 let makeButton = document.querySelector('.make');
 let randomizeToggle = false;
 let randomizeButton = document.querySelector('.randomize')
+let mouseCount = [];
 
 // Make container on load
 makeContainer();
@@ -26,6 +27,7 @@ function makeContainer () {
         let division = (100 / rows).toFixed(2);
         let box = document.createElement('div');
         box.classList.add('box');
+        box.dataset.index = i;
         box.style.flexBasis = `${division}%`;
         container.appendChild(box);
     }
@@ -35,16 +37,21 @@ function makeContainer () {
 
 function setEvents () {
     let boxes = document.querySelectorAll('.box');
+    let index = 0;
     for (box of boxes) {
+        mouseCount[index] = 0;
         box.addEventListener("mouseover", (e) => {
+            let number = e.target.dataset.index;
+            mouseCount[number] += 0.1;
             if (randomizeToggle) {
                 let color = getRandomColor();
                 e.target.style.background = `${color}`;
             }
             else {
-                e.target.style.background = `black`;
+                e.target.style.background = `rgba(0,0,0, ${mouseCount[number]})`;
             }
         })
+        index++;
     }
 }
 
